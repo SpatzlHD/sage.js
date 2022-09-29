605980375c49e32b28c54da9e3c0941d4bed52a9
SpatzlHD <alexander.koegel1111@gmail.com>
2022-09-29 08:30:15 +0200

    improved error handeling

--- handler/apiErrorHandler.js
+++ handler/apiErrorHandler.js
@@ -6,26 +6,41 @@ class ApiErrorHandler {
 
   handle() {
     const e = this.error;
-    if (e.response.status === 403) {
+    const status = e.response.status;
+    if (status === 400) {
+      throw new Error(
+        `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that there is a syntax error in the request and the request has therefore been denied.`
+      );
+    } else if (status === 401) {
+      throw new Error(
+        `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that the request being made did not contain the necessary authentication credentials (e.g., an API key) and therefore the client was denied access.`
+      );
+    } else if (status === 403) {
       if (this.isProductionKey) {
         throw new Error(
-          `You are trying to use a development key, please use a production key to use this endpoint\n\nRiot API Error message: ${e.response.data.status.message}`
+          `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that the server understood the request but refuses to authorize it. There is no distinction made between an invalid path or invalid authorization credentials (e.g., an API key). You are probably using a development key on a production key only endpoint.`
         );
       } else {
         throw new Error(
-          `You are trying to use a invalid api key please check your api key\n\nRiot API Error message: ${e.response.data.status.message}`
+          `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that the server understood the request but refuses to authorize it. There is no distinction made between an invalid path or invalid authorization credentials (e.g., an API key).`
         );
       }
     } else if (e.response.status === 404) {
       throw new Error(
-        `The Content you are trying to get is not available\nTry to check your query\n\nRiot API Error message: ${e.response.data.status.message}`
+        `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that the server has not found a match for the API request being made. No indication is given whether the condition is temporary or permanent.`
       );
     } else if (e.response.status === 429) {
       throw new Error(
-        `You are being rate limited by Riot Games\nTry again later\n\nRiot API Error message: ${e.response.data.status.message}`
+        `Status Code: ${status} - ${e.response.data.status.message}\n\nThis error indicates that the application has exhausted its maximum number of allotted API calls allowed for a given duration.\n\nTry again in ${e.response.headers["retry-after"]} seconds.`
       );
     } else if (e.response.status === 500) {
-      throw new Error("Riot Games Servers are currently down\nTry again later");
+      throw new Error(
+        `${status} - ${e.response.data.status.message}\n\nThis error indicates an unexpected condition or exception which prevented the server from fulfilling an API request.`
+      );
+    } else if (status === 503) {
+      throw new Error(
+        `${status} - ${e.response.data.status.message}\n\nThis error indicates the server is currently unavailable to handle requests because of an unknown reason. The Service Unavailable response implies a temporary condition which will be alleviated after some delay.`
+      );
     } else {
       throw new Error(`An unknown error occured\n${e}`);
     }
